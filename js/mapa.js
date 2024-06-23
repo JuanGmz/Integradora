8c6f0bd0288fd959257f7b2d95a7afaa88fea1f7
JuanGmz <juanalfredohd@gmail.com>
2024-06-23 12:29:56 -0600

    Se agrego sombra y titulo al formulario de contacto

--- js/mapa.js
+++ js/mapa.js
@@ -3,8 +3,6 @@ function iniciarMap(){
     var map =new google.maps.Map(document.getElementById('map'),{
         zoom:18,
         center:coord
-
-
     });
     var marker =new google.maps.Marker({
         position: coord,
