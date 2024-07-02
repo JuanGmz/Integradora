e2bb14f7c6f41fe0e23273a331224cb6e0449e12
Noe Abel Vargas Lopez <noelopez191119@gmail.com>
2024-07-02 17:14:47 -0600

    Sp_AdminBuscarReserva.v1.1

--- BD/StorageProcedures.sql
+++ BD/StorageProcedures.sql
@@ -3,11 +3,12 @@
 delimiter //
 create procedure SP_BuscarAdminReserva(in folioreserva int)
 begin 
-select er.id_reserva as 'FOLIO RESERVA', er.estatus as ESTATUS, er.fecha_hora_reserva as 'FECHA / HORA RESERVA', e.nombre as EVENTO, e.fecha_evento as 'FECHA EVENTO',
-
+select 
+	concat(p.nombres,' ',p.apellido_paterno,' ', p.apellido_materno) as CLIENTE,er.id_reserva as 'FOLIO RESERVA', er.estatus as ESTATUS,
+    er.fecha_hora_reserva as 'FECHA / HORA RESERVA', e.nombre as EVENTO, e.fecha_evento as 'FECHA EVENTO'
 from eventos_reservas er
-join clientes c on c.id_cliente =er.id_cliente
-join personas p on p.id_persona = c.id_persona
-join eventos e on e.id_evento = er.id_evento;
+	join clientes c on c.id_cliente =er.id_cliente
+	join personas p on p.id_persona = c.id_persona
+	join eventos e on e.id_evento = er.id_evento where er.id_reserva = folioreserva;
 end //
 delimiter ;
