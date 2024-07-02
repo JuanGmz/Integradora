e1a31b255b6033e329eddceddb14ff75ecb727e8
Noe Abel Vargas Lopez <noelopez191119@gmail.com>
2024-07-02 17:12:53 -0600

    Sp_AdminBuscarReserva.v1.0

--- BD/StorageProcedures.sql
+++ BD/StorageProcedures.sql
@@ -1,6 +1,13 @@
+
+-- Buscar determinada reserva por folio.
 delimiter //
 create procedure SP_BuscarAdminReserva(in folioreserva int)
 begin 
+select er.id_reserva as 'FOLIO RESERVA', er.estatus as ESTATUS, er.fecha_hora_reserva as 'FECHA / HORA RESERVA', e.nombre as EVENTO, e.fecha_evento as 'FECHA EVENTO',
 
+from eventos_reservas er
+join clientes c on c.id_cliente =er.id_cliente
+join personas p on p.id_persona = c.id_persona
+join eventos e on e.id_evento = er.id_evento;
 end //
 delimiter ;
