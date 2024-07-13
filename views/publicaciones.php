743ef39856e8c27d54187c860b8a21d419580549
JuanGmz <juanalfredohd@gmail.com>
2024-07-12 20:48:56 -0600

    Ya se muestran las publicaciones

--- views/publicaciones.php
+++ views/publicaciones.php
@@ -17,7 +17,8 @@
                 <a class="navbar-brand" href="../index.php">
                     <img src="../img/Sinfonía-Café-y-Cultura.webp" alt="Logo" class="logo" loading="lazy">
                 </a>
-                <div class="offcanvas offcanvas-end" style="background: var(--primario);" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas offcanvas-end" style="background: var(--primario);" tabindex="-1"
+                    id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div class="offcanvas-header">
                         <h5 class="offcanvas-title text-light fw-bold" id="offcanvasNavbarLabel">SifoníaCafé&Cultura
                         </h5>
@@ -47,108 +48,61 @@
                     </div>
                 </div>
                 <a href="login.php" class="login-button ms-auto">Iniciar Sesión</a>
-                <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </div>
         </nav>
         <!-- NavBar End -->
+
         <div class="container mb-5">
-            <div class="text-center fw-bold fs-2 mt-5 mb-3">
+            <div class="fw-bold fs-2 mt-3 mb-4">
                 <h1 class="h1contact">Publicaciones</h1>
             </div>
-        </div>
+            <h2>Conoce Recetas, Secretos y Más Sobre el Café!</h2>
 
-        <div class="col-12 p-5">
-            <div class="row g-4 ">
-
-                <?php
-                include_once("../class/database.php");
-
-                // Conectar a la base de datos
-                $conexion = new Database();
-                $conexion->conectarDB();
-
-                // Configurar la paginación
-                $results_per_page = 9; // Número de resultados por página
-                $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
-                $page = max($page, 1); // Asegurar que la página sea al menos 1
-
-                // Contar el número total de publicaciones
-                $total_query = 'SELECT COUNT(*) AS total FROM publicaciones WHERE tipo = "Blog"';
-                $total_result = $conexion->select($total_query);
-                $total_pages = ceil($total_result[0]->total / $results_per_page);
-
-                // Calcular el desplazamiento (offset)
-                $offset = ($page - 1) * $results_per_page;
-
-                // Obtener las publicaciones para la página actual
-                $query = 'SELECT 
-                titulo, 
-                descripcion, 
-                img_url
-                FROM 
-                publicaciones
-                WHERE 
-                tipo = "Blog"
-                LIMIT ' . $offset . ', ' . $results_per_page;
-
-                $publicaciones = $conexion->select($query);
-                ?>
-
-                <div class="container">
-                    <div class="row g-4 p-3">
-                        <?php foreach ($publicaciones as $publicacion) : ?>
-                            <div class='col-md-4 col-12 col-sm-6'>
-                                <div class='card blog-card h-100'>
-                                    <img src='<?php echo $publicacion->img_url; ?>' class='card-img-top' alt='Image'>
-                                    <div class='card-body'>
-                                        <h5 class='blog-card-title'><?php echo $publicacion->titulo; ?></h5>
-                                        <h6 class='blog-card-subtitle mb-2 text-muted'>by Jane Doe / Competition / 23.02.2016</h6>
-                                        <p class='blog-card-text'><?php echo $publicacion->descripcion; ?></p>
-                                    </div>
-                                    <a href='#' class='blog-card-link p-3'>Mas detalles <i class='fa-solid fa-arrow-right'></i></a>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
+            <div class="row mb-3">
+                <div class="col-lg-6">
+                    <a href="publicaciones/blog.php">
+                    <div class="card mb-3">
+                        <img src="../img/cafes/cafe17.webp" class="card-img" alt="...">
+                        <div class="card-body">
+                            <h3 class="card-title fw-bold">Descubre el Maravilloso Mundo del Café en Nuestro Blog</h3>
+                            <p class="card-text">
+                                Bienvenidos a nuestro blog dedicado a la bebida que despierta pasiones y 
+                                energiza nuestros días: el café. Desde los rituales matutinos hasta las 
+                                pausas necesarias en la jornada laboral, el café se ha convertido en una 
+                                parte esencial de nuestras vidas. En este espacio, exploraremos todo lo 
+                                relacionado con esta fascinante bebida, desde su rica historia y los diversos 
+                                métodos de preparación, hasta las mejores variedades de granos y las últimas tendencias en el mundo cafetero.
+                            </p>
+                            <a href='publicaciones/blog.php' class='blog-card-link p-3'>Mas detalles <i class='fa-solid fa-arrow-right'></i></a>
+                        </div>
                     </div>
+                    </a>
                 </div>
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination d-flex justify-content-center">
-                        <li class="page-item<?php if ($page <= 1) {
-                                                echo ' disabled';
-                                            } ?>">
-                            <a class="page-link custom-page-link" href="?page=<?php echo $page - 1; ?>" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                            </a>
-                        </li>
-                        <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
-                            <li class="page-item custom-page-item<?php if ($page == $i) {
-                                                                        echo ' active';
-                                                                    } ?>">
-                                <a class="page-link custom-page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
-                            </li>
-                        <?php endfor; ?>
-                        <li class="page-item<?php if ($page >= $total_pages) {
-                                                echo ' disabled';
-                                            } ?>">
-                            <a class="page-link custom-page-link" href="?page=<?php echo $page + 1; ?>" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-
-                <?php
-                $conexion->desconectarDB();
-                ?>
-
+                <div class="col-lg-6">
+                    <a href="publicaciones/blog.php">
+                    <div class="card mb-3">
+                        <img src="../img/cafes/cafe17.webp" class="card-img" alt="...">
+                        <div class="card-body">
+                            <h3 class="card-title fw-bold">Bienvenidos a SifoníaCafé&Cultura, Nuestro Canal de Difusión</h3>
+                            <p class="card-text">
+                                Hola amantes del café y curiosos del buen sabor, ¡bienvenidos a SifoníaCafé&Cultura! 
+                                Este es el lugar perfecto para todos aquellos que comparten nuestra pasión por esta 
+                                increíble bebida que nos acompaña en tantos momentos de nuestras vidas. Desde los orígenes 
+                                del café y sus fascinantes historias, hasta los métodos más innovadores de preparación y 
+                                las últimas tendencias en el mundo cafetero, aquí encontrarás contenido para todos los gustos.
+                            </p>
+                            <a href='publicaciones/difusion.php' class='blog-card-link p-3'>Mas detalles <i class='fa-solid fa-arrow-right'></i></a>
+                        </div>
+                    </div>
+                    </a>
+                </div>
             </div>
         </div>
-
-
     </div>
-
 
     <!-- Footer -->
     <footer>
