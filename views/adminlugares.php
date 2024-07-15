dfb417ee1bb7d9be70d90c92bf0acfe694f159bb
IKER1208 <iker34267@gmail.com>
2024-07-14 21:26:13 -0600

    texto

--- views/adminlugares.php
+++ views/adminlugares.php
@@ -470,7 +470,7 @@
                                                     <div class='modal-dialog'>
                                                         <div class='modal-content'>
                                                             <div class='modal-header'>
-                                                                <h1 class='modal-title fs-5' id='exampleModalLabel'>Editar Producto</h1>
+                                                                <h1 class='modal-title fs-5' id='exampleModalLabel'>Editar Lugar</h1>
                                                                 <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
                                                             </div>
                                                             <!-- Aquí va el contenido del modal -->
