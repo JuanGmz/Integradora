a4fb43d1c0a6d812099d56439ced03d807d5d207
JuanGmz <juanalfredohd@gmail.com>
2024-08-03 19:54:49 -0600

    Correccion index

--- index.php
+++ index.php
@@ -136,7 +136,7 @@
                                 La primera barra y expendio de café de especialidad en la laguna.
                                 Café 100% Mexicano seleccionado por nuestro maestro catador y tostador. ☕️♥️
                             </p>
-                            <a href="views/conocenos.php" class="btn btn-cafe ">Ver mas</a>
+                            <a href="views/conocenos.php" class="btn btn-cafe ">Ver Más</a>
                         </div>
                     </div>
                 </div>
