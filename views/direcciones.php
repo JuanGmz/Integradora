7241d47dfb534156a99998d968dac5a73685a279
IKER1208 <iker34267@gmail.com>
2024-08-03 19:11:30 -0600

    olaa

--- views/direcciones.php
+++ views/direcciones.php
@@ -420,6 +420,12 @@ if (isset($_SESSION["usuario"])) {
             return new bootstrap.Popover(popoverTriggerEl)
         })
     </script>
+    <script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://kit.fontawesome.com/45ef8dbe96.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
 </body>
 
