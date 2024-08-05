7d883b84eccbefff548fbaf0a2390abc155b73f1
noahx001 <lopeznoe191119@gmail.com>
2024-08-05 14:46:36 -0600

    correccion a admin eventos mayusculas a minisuculas

--- views/adminEventos.php
+++ views/adminEventos.php
@@ -24,7 +24,7 @@ if (isset($_POST['btnactualizar'])) {
     $id_evento = intval($id_evento);
 
     // Construir la consulta de actualización
-    $consultapago = "UPDATE EVENTOS SET  
+    $consultapago = "UPDATE eventos SET  
         nombre = '$nombre',
         descripcion = '$descripcion',
         fecha_evento = '$fechaEvento', 
@@ -36,7 +36,7 @@ if (isset($_POST['btnactualizar'])) {
         fecha_publicacion = '$fechaPub'
         WHERE id_evento = '$id_evento'";
 
-    $consultagratuito = "UPDATE EVENTOS SET  
+    $consultagratuito = "UPDATE eventos SET  
         nombre = '$nombre',
         descripcion = '$descripcion',
         fecha_evento = '$fechaEvento', 
@@ -111,9 +111,9 @@ if (isset($_POST['btnactualizar'])) {
                 <div class="accordion accordion-flush" id="accordionMobile">
                     <div class="accordion-item m-0 p-0 row">
                         <h2 class="accordion-header">
-                            <button class="row accordion-button collapsed fw-bold fs-4 bagr-cafe4 text-light" type="button"
-                                data-bs-toggle="collapse" data-bs-target="#flush-inicio" aria-expanded="false"
-                                aria-controls="flush-inicio">
+                            <button class="row accordion-button collapsed fw-bold fs-4 bagr-cafe4 text-light"
+                                type="button" data-bs-toggle="collapse" data-bs-target="#flush-inicio"
+                                aria-expanded="false" aria-controls="flush-inicio">
                                 <div class="col-6">
                                     <a href="adminInicio.php" class="text-light fw-bold text-decoration-none">
                                         <i class="fa-solid fa-house-laptop me-1"></i>
@@ -582,8 +582,7 @@ if (isset($_POST['btnactualizar'])) {
                                     </div>
 
                                     <div class="col-4">
-                                        <button type="submit" class="btn btn-dark w-100"
-                                            value="Buscar">Buscar</button>
+                                        <button type="submit" class="btn btn-dark w-100" value="Buscar">Buscar</button>
                                     </div>
                                 </div>
                             </form>
